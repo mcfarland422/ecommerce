071dc4b4a8b6f0cca59e13188444557a59033c16
Michael McFarland <gardengrove422@gmail.com>
2017-12-11 12:29:13 -0500

    updated

--- back-end/app.js
+++ back-end/app.js
@@ -9,8 +9,16 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+// This is an API. We need to let browsers know it's OK to request stuff, we also
+// need to set our API up to only send certain responses
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-// view engine setup  --- NO VIEWS 
+
+// view engine setup  --- NO VIEWS
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
---
+++ front-end/src/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <h1>NavBar Sanity Check, there is no sanity here</h1>
+    )
+  }
+}
+
+export default NavBar;
